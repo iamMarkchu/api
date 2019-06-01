2732f960c56d3e69c4e8dab648756c97f7a4e135
褚魁 <18627032049@163.com>
2019-06-01 23:52:06 +0800

    [feat] kchu: 使用go mod管理依赖，完善DockerFile，注解路由加入版本控制

---
+++ routers/commentsRouter_________________________Users_mark_go_src_api_controllers.go
@@ -0,0 +1,100 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["api/controllers:ArticleController"] = append(beego.GlobalControllerRouter["api/controllers:ArticleController"],
+        beego.ControllerComments{
+            Method: "Index",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:ArticleController"] = append(beego.GlobalControllerRouter["api/controllers:ArticleController"],
+        beego.ControllerComments{
+            Method: "Store",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:ArticleController"] = append(beego.GlobalControllerRouter["api/controllers:ArticleController"],
+        beego.ControllerComments{
+            Method: "Update",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:ArticleController"] = append(beego.GlobalControllerRouter["api/controllers:ArticleController"],
+        beego.ControllerComments{
+            Method: "Show",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:ArticleController"] = append(beego.GlobalControllerRouter["api/controllers:ArticleController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:ArticleController"] = append(beego.GlobalControllerRouter["api/controllers:ArticleController"],
+        beego.ControllerComments{
+            Method: "Publish",
+            Router: `/:id/publish`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:CategoryController"] = append(beego.GlobalControllerRouter["api/controllers:CategoryController"],
+        beego.ControllerComments{
+            Method: "Index",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:CategoryController"] = append(beego.GlobalControllerRouter["api/controllers:CategoryController"],
+        beego.ControllerComments{
+            Method: "Store",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:PublicController"] = append(beego.GlobalControllerRouter["api/controllers:PublicController"],
+        beego.ControllerComments{
+            Method: "Login",
+            Router: `/login`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:PublicController"] = append(beego.GlobalControllerRouter["api/controllers:PublicController"],
+        beego.ControllerComments{
+            Method: "Register",
+            Router: `/register`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
